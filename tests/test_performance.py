34607c2450bed2b964aa6f67ecf69396bdfe167c
mkudmi <mku.dmi@gmail.com>
2023-04-14 17:47:48 +0200

    add_put_delete_perf_tests

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -25,3 +25,29 @@ def test_post_item_performance():
     assert response.status_code == 200
     assert response.elapsed.total_seconds() < 1.0
     print("Response time: {} seconds".format(response.elapsed.total_seconds()))
+
+#Тест на проверку скорости PUT запроса. Выводит кол-во секунд
+@pytest.mark.performance
+def test_put_item_performance():
+    payload = {
+        "id": 101,
+        "fname": "Jane",
+        "lname": "Doe",
+        "phone": "+1234567890",
+        "bday": "1980-01-01"
+    }
+    response = requests.put(BASE_URL, json=payload)
+    assert response.status_code == 200
+    assert response.elapsed.total_seconds() < 1.0
+    print("Response time: {} seconds".format(response.elapsed.total_seconds()))
+    
+#Тест на проверку скорости POST запроса. Выводит кол-во секунд
+@pytest.mark.performance
+def test_delete_item_performance():
+    payload = {
+        "id": 101
+    }
+    response = requests.delete(BASE_URL, json=payload)
+    assert response.status_code == 200
+    assert response.elapsed.total_seconds() < 1.0
+    print("Response time: {} seconds".format(response.elapsed.total_seconds()))
