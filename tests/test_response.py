a99ce6fa3b5711ad4e88d75add833fef46328f46
mkudmi <mku.dmi@gmail.com>
2023-04-14 14:32:11 +0200

    add_post_get_tests

---
+++ tests/test_response.py
@@ -0,0 +1,31 @@
+import pytest
+import requests
+
+BASE_URL = 'http://localhost:8180'
+
+#Тест на проверку GET запроса. Проверяет статус код, возврат параметра total и data.
+def test_get_phonebook():
+    try:
+        response = requests.get(f'{BASE_URL}/')
+        assert response.status_code == 200
+        assert "total" in response.json()
+        assert "data" in response.json()
+        print("Status code: {}. SUCCESS".format(response.status_code))
+    except AssertionError:
+        print("Status code: {}. FAILED. Must be 200".format(response.status_code))
+
+#Тест на проверку POST запроса. Проверяет статус кода, возврат параметра success.
+def test_create_person():
+    payload = {
+        "fname": "John",
+        "lname": "Doe",
+        "phone": "+1234567890",
+        "bday": "1990-01-01"
+    }
+    try:
+        response = requests.post(f'{BASE_URL}/', json=payload)
+        assert response.status_code == 201
+        assert "success" in response.json()
+        print("Status code: {}. SUCCESS".format(response.status_code))
+    except AssertionError:
+        print("Status code: {}. Must be 201".format(response.status_code))
