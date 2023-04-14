194783fe1806adcb4228b0f736a25e076bb52cdf
mkudmi <mku.dmi@gmail.com>
2023-04-14 18:01:34 +0200

    add_inalid_post_fname_response

--- tests/test_negative_invalid_data.py
+++ tests/test_negative_invalid_data.py
@@ -15,3 +15,19 @@ def test_get_items_with_invalid_id():
         print("Status code: {}. SUCCESS".format(response.status_code))
     except:
         print("Status code: {}. FAILED. Must be 400".format(response.status_code))
+
+#Тест POST запроса с int fname
+def test_create_person_with_invalid_fname():
+    try:
+        payload = {
+            "id": "103",
+            "fname": "123",
+            "lname": "Doe",
+            "phone": "+1234567890",
+            "bday": "1990-01-01"
+        }
+        response = requests.post(f'{BASE_URL}/', json=payload)
+        assert response.status_code == 400
+        print("Status code: {}. Person is not created".format(response.status_code))
+    except:
+        print("Status code: {}. Person created. Must be 400".format(response.status_code))
